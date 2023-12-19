380d11ba1aa6dbc2f81b9113b68c268dd01377da
daqian <daqiancode@gmail.com>
2023-12-19 10:03:31 +0400

    add get ext

--- zbase/utils/files.py
+++ zbase/utils/files.py
@@ -1,4 +1,4 @@
-import json
+import json,os
 
 def read_lines(file:str, parse_json = False, skip_empty=True):
     with open(file , 'r') as f:
@@ -10,3 +10,27 @@ def read_lines(file:str, parse_json = False, skip_empty=True):
                 yield json.loads(line)
             else:
                 yield line.strip()
+
+
+def get_ext(filename:str)->str:
+    """
+    Get file extension.
+    """
+    if "." not in filename:
+        return ""
+    filename = os.path.basename(filename)
+    filename = filename.rstrip().lower()
+    if filename.endswith(".tar.gz"):
+        return "tar.gz"
+    return os.path.splitext(filename)[-1].lstrip(".")
+
+
+
+def get_filename_without_ext(filename:str)->str:
+    """
+    Get filename without extension.
+    """
+    if "." not in filename:
+        return filename
+    filename = os.path.basename(filename)
+    return os.path.splitext(filename)[0]
